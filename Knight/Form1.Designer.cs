60e7275328179f2cc38f777c221c8b0a5d7f02f9
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-04-04 21:41:43 +0200

    add game map and hotkeys

--- Knight/Form1.Designer.cs
+++ Knight/Form1.Designer.cs
@@ -28,12 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gameMap = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(511, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newGameToolStripMenuItem.Text = "New game";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // gameMap
+            // 
+            this.gameMap.ColumnCount = 2;
+            this.gameMap.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45.37445F));
+            this.gameMap.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 54.62555F));
+            this.gameMap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameMap.Location = new System.Drawing.Point(0, 24);
+            this.gameMap.Margin = new System.Windows.Forms.Padding(0);
+            this.gameMap.Name = "gameMap";
+            this.gameMap.RowCount = 2;
+            this.gameMap.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.gameMap.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.gameMap.Size = new System.Drawing.Size(511, 367);
+            this.gameMap.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(511, 391);
+            this.Controls.Add(this.gameMap);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The Knight";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel gameMap;
     }
 }
 
--- Knight/Form1.cs
+++ Knight/Form1.cs
@@ -12,9 +12,90 @@ namespace Knight
 {
     public partial class Form1 : Form
     {
+        private int gameRows = 8;
+        private int gameColumns = 8;
+
         public Form1()
         {
             InitializeComponent();
+            ResetGameMap();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show(this, "Are you sure?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.None) != DialogResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void ResetGameMap()
+        {
+            gameMap.CellPaint -= GameMap_CellPaint;
+
+            gameMap.Controls.Clear();
+
+            float rowHeightPercentage = 100 / gameRows;
+            gameMap.RowCount = gameRows;
+            gameMap.RowStyles.Clear();
+            for (int i=0; i < gameRows; i++)
+            {
+                gameMap.RowStyles.Add(new RowStyle(SizeType.Percent, rowHeightPercentage));
+            }
+
+            float columnWidthPercentage = 100 / gameColumns;
+            gameMap.ColumnCount = gameColumns;
+            gameMap.ColumnStyles.Clear();
+            for (int i = 0; i < gameColumns; i++)
+            {
+                gameMap.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, columnWidthPercentage));
+            }
+
+            Random r = new Random();
+            for (int y = 0; y < gameRows; y++)
+            {
+                for (int x = 0; x < gameColumns; x++)
+                {
+                    Panel panel = new Panel();
+                    panel.BackColor = r.Next(2) == 0 ? Color.Maroon : Color.ForestGreen;
+                    panel.Dock = DockStyle.Fill;
+                    gameMap.Controls.Add(panel, x, y);
+                    
+                }
+            }
+
+            gameMap.CellPaint += GameMap_CellPaint;
+        }
+
+        private void GameMap_CellPaint(object sender, TableLayoutCellPaintEventArgs e)
+        {
+            Control c = gameMap.GetControlFromPosition(e.Column, e.Row);
+            if (c == null)
+                return;
+            using (var b = new SolidBrush(c.BackColor))
+            {
+                e.Graphics.FillRectangle(b, e.CellBounds);
+            }
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Control | Keys.N))
+            {
+                ResetGameMap();
+                return true;
+            }
+            else if (keyData == (Keys.Control | Keys.M))
+            {
+#warning TODO: menu
+                return true;
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
         }
     }
 }
